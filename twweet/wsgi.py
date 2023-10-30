4986434d8a90d6498cb42ee3f0ab1280b97a9245
Nikhil <its.nikhil2808@gmail.com>
2023-10-30 07:47:40 +0530

    started

---
+++ twweet/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for twweet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twweet.settings')
+
+application = get_wsgi_application()
